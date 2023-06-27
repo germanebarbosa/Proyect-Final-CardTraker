fc40255d78d6981814625814060b22adc4b06b53
julian19028 <121776944+julian19028@users.noreply.github.com>
2023-06-27 02:39:58 +0000

    Doing API

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,18 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Stablishments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stablishments_name = db.Column(db.String(120), unique=True, nullable=False)
+    stablishments_links = db.Column(db.String(80), unique=False, nullable=False)
+    status = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Stablishments {self.stablishments}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "stablishment": self.stablishments_name,
+        }    
