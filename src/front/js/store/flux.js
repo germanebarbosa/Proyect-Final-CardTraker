a17a92e8fa1430dc5cab8b0ee76c7bb87ea2423f
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-06-27 02:59:17 +0000

    Agregue casi todo el back del login y parte del front, hace falta adicionar los endpoints para editar la informacion del usuario y verificar bien el tema de la autorizacion con el token.

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,12 +13,51 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			auth: false
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+
+			login: (email,password) => {
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type':'application/json'},
+					body: JSON.stringify(
+						{
+							"email": email,
+							"password": password
+						}
+					)
+				};
+				fetch("https://germanebarbosa-symmetrical-meme-44g9rj7q7g7375pr-3001.preview.app.github.dev/api/login", requestOptions)
+					.then(response => {
+						console.log(response.status); //imprimo la validacion del codigo, 200 es correcto 401 significa error.
+						if( response.status === 200 ){
+								setStore({auth: true}) // Modifico el valor de la variable auth.
+							}else return alert("Usuario o clave incorrecto")
+						return response.json()
+					})
+					.then(data => {
+						localStorage.setItem("token", data.access_token)
+						console.log(data)
+					});
+			},
+
+			signup: (email,user_name,password) => {
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type':'application/json'},
+					body: JSON.stringify(
+						{
+							"email": email,
+							"user_name": user_name,
+							"password": password
+						}
+					)
+				};
+				fetch("https://germanebarbosa-symmetrical-meme-44g9rj7q7g7375pr-3001.preview.app.github.dev/api/signup", requestOptions)
+					.then(response =>response.json())
+					.then(data => console.log(data));
 			},
 
 			getMessage: async () => {
